8111524adeb2108774833d2eebfc1523224e2dc4
ocman nazir briet <usmannazirbareet@gmail.com>
2023-10-07 00:59:01 +0500

    added shapes code

--- shapes.py
+++ shapes.py
@@ -96,3 +96,49 @@ side = 120
 for i in range(5):
 	form_hex(side)
 	side -= 12
+
+
+# # Turtle Star 
+from turtle import *
+import turtle as tur
+import sys
+from time import sleep
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def normal_star(siz, clr, point):
+    if point <= 4:
+        raise ValueError('Not enough point')
+
+    tur.color(clr)
+
+    for coprime in range(point // 2, 1, -1):
+        if gcd(point, coprime) == 1:
+
+            print("({},{})".format(point, coprime), file=sys.stderr)
+
+            start = tur.position()
+
+            for _ in range(point):
+                tur.forward(siz)
+                tur.left(360.0 / point * coprime)
+
+            tur.setposition(start)
+
+            return
+
+    abnormal_star(siz, clr, point)
+
+def abnormal_star(siz, clr, point):
+    
+    print("Exception:", point, file=sys.stderr)
+
+for point in range(7, 20):
+    tur.reset()
+    normal_star(250, 'blue', point)
+    sleep(6)
+
+tur.exitonclick()
